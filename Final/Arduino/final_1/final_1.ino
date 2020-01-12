0b054143a0f21ad5b6bc9f2a3a76be58a4cef894
Ｍac <chinyi0523@acde-MacBook-Pro.local>
2020-01-12 15:14:02 +0800

    delete unuse1

--- Final/Arduino/final_1/final_1.ino
+++ Final/Arduino/final_1/final_1.ino
@@ -45,12 +45,9 @@ void loop()
   now[4] = get_strength(flex_pin5);
   //Serial.println("  --Start--Touch--");
   TestEvent(touchr1,_1pressed);
-  
-  ////
   //printTouchStatus(1,_1pressed);
   for(int i =0; i<4;i++)
     dimension[i]= false; 
-
   if(_1pressed[1]||_1pressed[2]||_1pressed[6]||_1pressed[5]) dimension[3]=true;
   if(_1pressed[3]||_1pressed[4]||_1pressed[7]||_1pressed[8]) dimension[0]=true;
   if(_1pressed[9]||_1pressed[10]||_1pressed[13]||_1pressed[14]) dimension[2]=true;
@@ -61,48 +58,11 @@ void loop()
   Serial.print(dimension[1]);
   Serial.print(dimension[2]);
   Serial.println(dimension[3]);
-  ////
-  //
-  //Serial.println("  --Start--ECG--");
-  //int ECG = analogRead(A0);
+
   //Serial.println("  --Start--Analyze--");
   for(int i=0;i<5;i++){
     strength[i] = push_strength(now[i]-prev[i]);
-    
   }
-  //////ASCII NUM to Signal
-  //01-16 RIGHT_FIRST
-  //17-32 RIGHT_SECOND
-  //33-48 RIGHT_THIRD  
-  //49-64 LEFT
-  //65-69 THUMB 1-5
-  //70-74 FORE FINGER 1-5
-  //75-79 MIDDLE FINGER 1-5
-  //80-84 RING FINGER 1-5
-  //85-89 LITTLE FINGER 1-5
-  //90-91 LEFTLEG EMG
-  //92-93 RIGHTLEG EMG 
-  //94    ECG
-  //00    RESERVED
-  ///////////////////////////
-  /*
-  bool SIGNALS[95]={false};
-  for(int i=0;i<5;i++){
-    if(strength[i]>0){
-      SIGNALS[64+5*i+strength[i]]=true;
-    }
-  }
-  for(int i=1;i<5;i++){
-    SIGNALS[i]=dimension[i-1];  
-  }
-  SIGNALS[5]=dimension[1]&&dimension[2];
-  SIGNALS[6]=dimension[1]&&dimension[3];
-  SIGNALS[7]=dimension[1]&&dimension[4];
-  SIGNALS[8]=dimension[2]&&dimension[3];
-  SIGNALS[9]=dimension[2]&&dimension[4];
-  SIGNALS[10]=dimension[3]&&dimension[4];
-  //if(ECG>900) SIGNALS[94]=true;
-  */
   Serial.print('s');
   Serial.print(strength[0]);
   Serial.print(strength[1]);
@@ -110,7 +70,6 @@ void loop()
   Serial.print(strength[3]);
   Serial.println(strength[4]);
   
-  //TODO: pass by USB
   delay(25);
   for(int i=0;i<5;i++){
     prev[i]=now[i];
@@ -131,7 +90,6 @@ void printTouchStatus(int num,bool* save)
       Serial.print(i);
       Serial.print(" ");
     }
-    
   }
   Serial.println(" |");
 }
